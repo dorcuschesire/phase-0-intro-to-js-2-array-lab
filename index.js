f711dbb5d7c833b48cc85c6dada1444ea2264b5d
dorcuschesire <chesiredorcus@gmail.com>
2024-07-02 15:20:24 +0300

    index.js, results.json

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+
+function appendCat(Broom){
+    return[...cats,Broom];
+}
+function prependCat(Arnold){
+    return[Arnold,...cats]
+}
+function removeLastCat(){
+    return cats.slice(0, 2)
+}
+function removeFirstCat(){
+    return cats.slice(1)
+}
